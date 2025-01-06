6b5181c2545682a77c4462e3d04ff01d0aadadf0
Senia Babenko <senia.babenko@gmail.com>
2025-01-06 21:18:05 +0300

    home changed to index

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
     path('<int:post_id>/', views.post_detail, name='post_detail'),
     path('<slug:category_slug>/', views.category_posts, name='category_posts'),
 ]
